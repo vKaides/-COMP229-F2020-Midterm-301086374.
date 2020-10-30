d15d3e0dab0b5f8453448594282251c760c3a5af
vkaides <aorata0903@gmail.com>
2020-10-30 17:29:14 -0400

    updated db URI to work with MongoDb Atlas

--- COMP229-F2020-MidTerm-Start/server/config/db.js
+++ COMP229-F2020-MidTerm-Start/server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+ // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Scott:gGGSUElRteSKLYlA@cluster0.2eiyj.mongodb.net/book_store?retryWrites=true&w=majority"
 };
